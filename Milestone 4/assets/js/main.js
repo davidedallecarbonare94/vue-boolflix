3171bc00caca7e5e5a7d8cc61e9b9ebe7619452e
davidedallecarbonare94 <davide.dallecarbonare94@gmail.com>
2021-06-09 12:50:11 +0200

    start milestone 4

---
+++ Milestone 4/assets/js/main.js
@@ -0,0 +1,28 @@
+var root = new Vue({
+    el: "#root",
+    data: {
+        series: "",
+        films: "",
+        query: "",
+        url: "https://api.themoviedb.org/3",
+        api_key: "30db9530ead0b63d24c933102d9ad862",
+    },
+    methods: {
+        searchAFilm: function () {
+            axios.get(`${this.url}/search/movie?api_key=${this.api_key}&query=${this.query}`)
+            .then(result => {
+                this.films = result.data.results;
+                console.log(this.films);
+            })
+            axios.get(`${this.url}/search/tv?api_key=${this.api_key}&query=${this.query}`)
+                .then(res => {
+                    this.series = res.data.results;
+                    console.log(this.series);
+                })
+        }
+    },
+    mounted() {
+        
+    }
+    
+})
